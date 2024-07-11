3131138aa7c8be7e9a0a69d4eb3a52f827c77e32
Victor Vinicius <victormafiavinicius@gmail.com>
2024-07-11 12:00:13 -0300

    Implementando Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,58 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// Meus componentes
-import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
-import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
-import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"; 
+import { AppComponent } from './app.component';
 import { CategoriasComponent } from './categorias/categorias.component';
-import { LoginComponent } from './login/login.component';
+import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
+import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component'; 
 import { LogoutComponent } from './logout/logout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input'; 
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    CategoriaDetalheComponent,
-    CategoriaEditarComponent,
-    CategoriaNovaComponent,
+    AppComponent,
     CategoriasComponent,
-    LoginComponent,
-    LogoutComponent
+    CategoriaDetalheComponent,
+    CategoriaNovaComponent,
+    CategoriaEditarComponent,
+    MenuComponent, 
+    LogoutComponent,
+    LoginComponent
   ],
   imports: [
-    CommonModule 
-  ]
+    CommonModule, 
+    AppModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatCardModule,
+    MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
